9aebf3d0476a1678c43128a265bfa533f573ca8d
Christian <christiangutierrezramirez@gmail.com>
2024-06-05 21:51:30 -0500

    Creacion de carpetas y archivo models.py

---
+++ models/models.py
@@ -0,0 +1,8 @@
+#modelos para el proyecto Bitacora
+
+#Clase flight
+#atributos fecha, origen, destino, duracion, observacion, avion
+
+
+#Clase aircraft
+#Atributes Manufacter, tail number, Tipe of engines, Hp engines
